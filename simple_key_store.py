bcdfd242a4fcb9dcd80d35af930e1882de9a9d32
Paul <paul@nowfree.org>
2024-07-31 15:15:59 +0000

    added updated_at, created_at. Working on get_next_usable_key

--- simple_key_store.py
+++ simple_key_store.py
@@ -15,6 +15,8 @@ class SimpleKeyStore:
         self.KEYSTORE_TABLE_NAME = "keystore"
         self.create_keystore_table_if_dne()
 
+        self.set_defining_fields = ["name", "source", "login", "batch"]
+
     def __del__(self):
         self.close_connection()
 
@@ -54,7 +56,9 @@ class SimpleKeyStore:
                 batch TEXT, \
                 source TEXT, \
                 login TEXT, \
-                encrypted_key TEXT UNIQUE\
+                encrypted_key TEXT UNIQUE, \
+                created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+                updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP \
             )"
         )
         self.cx.commit()
@@ -290,7 +294,12 @@ class SimpleKeyStore:
         return cursor
 
     def tabulate_records(
-        self, records: List[Dict], headers: List = None, sort_order: List = None, show_full_key: bool = False, show_index : bool = True
+        self,
+        records: List[Dict],
+        headers: List = None,
+        sort_order: List = None,
+        show_full_key: bool = False,
+        show_index: bool = True,
     ) -> str:
         """Return a string of tabulated records. If headers is blank will use all keys. If given will sort by keys listed in sort_order."""
         # Extracting the keys to use as headers
@@ -370,7 +379,7 @@ class SimpleKeyStore:
                     headers=sort_order + ["expired", "usable", "key"],
                     sort_order=sort_order,
                     show_full_key=True,
-                    show_index= True,
+                    show_index=True,
                 )
             )
         return key_records
@@ -378,17 +387,14 @@ class SimpleKeyStore:
     def usability_counts_report(
         self, key_name: str = None, print_records: bool = False, print_counts=False
     ) -> List[Dict]:
-        
         usability_records = self.records_for_usability_report(key_name, print_records)
 
         # Count the number of usable records for each set, where a set is combo of name, source, login, batch
         set_records = self.get_sets_of_records_with_counts(usability_records)
-        usable_count = sum(1 for record in set_records if record.get('usable'))
+        usable_count = sum(1 for record in set_records if record.get("usable"))
 
         if print_counts:
-            print(
-                f"Usability counts ({len(usability_records)} records total, {usable_count} usable)"
-            )
+            print(f"Usability counts ({len(usability_records)} records total, {usable_count} usable)")
             print(self.tabulate_records(set_records, show_index=True))
 
         return set_records
@@ -438,7 +444,7 @@ class SimpleKeyStore:
             # Noting was given to set
             return
 
-        sql = f"UPDATE {self.KEYSTORE_TABLE_NAME} SET {', '.join(set_clause)} WHERE id = :id"
+        sql = f"UPDATE {self.KEYSTORE_TABLE_NAME} SET {', '.join(set_clause)}, updated_at = CURRENT_TIMESTAMP WHERE id = :id"
         cursor = self.cx.execute(sql, {"id": int(id_to_update), **params})
 
         if cursor.rowcount != 1:
@@ -454,13 +460,13 @@ class SimpleKeyStore:
     def get_sets_of_records_with_counts(self, records: List[Dict]):
         """Return a list of the record sets with their counts. Each set of records share name, source, login and batch."""
 
-        set_defining_fields = ["name", "source", "login", "batch"]
         count_fields = ["total", "active", "expired", "usable"]
 
         count_by_set_name = {}
+        min_upcoming_expiration_by_set_name = {}
         delim = "+|+"
         for record in records:
-            set_name = delim.join(str(record.get(field)) for field in set_defining_fields)
+            set_name = delim.join(str(record.get(field)) for field in self.set_defining_fields)
             if set_name not in count_by_set_name:
                 count_by_set_name[set_name] = {}
                 for cf in count_fields:
@@ -474,7 +480,18 @@ class SimpleKeyStore:
                 # Count each of active, expired, usable
                 count_by_set_name[set_name][cf] += 1 if record[cf] else 0
 
-        #print(f"{count_by_set_name=}")
+            # Keep track of the minimum upcoming (non-expired) expiration date for this set
+            if set_name not in min_upcoming_expiration_by_set_name:
+                min_upcoming_expiration_by_set_name[set_name] = None
+            if record["expired"]:
+                continue
+            if (
+                min_upcoming_expiration_by_set_name[set_name] is None
+                or record["expiration_date"] < min_upcoming_expiration_by_set_name[set_name]
+            ):
+                min_upcoming_expiration_by_set_name[set_name] = record["expiration_date"]
+
+        # print(f"{count_by_set_name=}")
 
         # Build a "set record" that will have each of the set defining fields and the counts
         set_records_list = []
@@ -484,19 +501,45 @@ class SimpleKeyStore:
             # Get the set defining field values from the set_name
             field_values = str(set_name).split(delim)
             i = 0
-            for field in set_defining_fields:
-                set_record[field] = field_values[i]
+            for field in self.set_defining_fields:
+                #print(f"{type(field_values[i])}, {field_values[i]=}")
+                set_record[field] = None if field_values[i] == 'None' else field_values[i]
                 i += 1
 
             # Add the counts
             for cf in count_fields:
                 set_record[cf] = count_by_set_name[set_name][cf]
 
+            # Add the min upcoming expiration date
+            set_record["upcoming_expiration"] = min_upcoming_expiration_by_set_name[set_name]
+
             # Save our set record
             set_records_list.append(set_record)
 
-        #print(f"{set_records_list=}")
+        # print(f"{set_records_list=}")
         return set_records_list
+
+    def record_is_in_set(self, record: Dict, record_set: Dict) -> bool:
+        """Returns true if record and set match on all the set defining fields"""
+        print("record_is_in_set checking:")
+
+        for field in self.set_defining_fields:
+            print(f"  checking {field}: {type(record.get(field))} {record.get(field)}, {type(record_set.get(field))} {record_set.get(field)}")
+            
+            if record.get(field) != record_set.get(field):
+                #print(f"    {record[field]} != {record_set[field]}")
+                return False
+            #print(f"    {record[field]} == {record_set[field]}")
+        return True
+
+    def get_set_for_record(self, record: Dict, record_set_list: List[Dict]) -> Dict:
+        """Returns the record set (Dict) of the set that matches the given record."""
+        for record_set in record_set_list:
+            if self.record_is_in_set(record, record_set):
+                print("Found record set for record")
+                return record_set
+        print("Did not find record set for record")
+        return None
 
     def get_next_usable_key(
         self,
@@ -505,8 +548,8 @@ class SimpleKeyStore:
         source: str = None,
         login: str = None,
     ):
-        """Return the next key to use that matches the given fields. Will look for:
-        1. Soonest expiring
+        """Return the next usable key that matches the given fields. Will look for:
+        1. Soonest expiring (by 12 hours)
         2. Smallest set of usable keys, where set is combo of name, source, login, batch"""
         matching_records = self.get_matching_key_records(
             name=name,
@@ -516,10 +559,48 @@ class SimpleKeyStore:
             login=login,
         )
 
-        # Get the usable records
-        usable_records = []
+        print(f"Got {len(matching_records)} matching records")
+        # Find the soonest upcoming expiration date
+        soonest_expiration = None
         for record in matching_records:
-            if record["usable"]:
-                usable_records.append(record)
+            if not record["usable"]:
+                continue
+            if soonest_expiration is None or record["expiration_date"] < soonest_expiration:
+                soonest_expiration = record["expiration_date"]
+        print(f"{soonest_expiration=}")
 
-        set_records = self.get_sets_of_records_with_counts(usable_records)
+        # Get the records that are soonest to expire (within 12 hours)
+        soon_to_expire_records = []
+        twelve_hours_in_seconds = 12 * 3600
+        for record in matching_records:
+            if record["expiration_date"] is None:
+                continue
+            time_diff = record["expiration_date"] - soonest_expiration
+            if time_diff < twelve_hours_in_seconds:
+                soon_to_expire_records.append(record)
+
+        print(self.tabulate_records(soon_to_expire_records))
+        # If only one record has soonest expiration just return it
+        if len(soon_to_expire_records) == 1:
+            return soon_to_expire_records[0]
+
+        if not soon_to_expire_records:
+            # None of the records have an expiration date
+            soon_to_expire_records = matching_records
+
+        # Break the "tie" by choosing the record with the least number of usable records in its set
+        record_set_list = self.get_sets_of_records_with_counts(matching_records)
+        print(f"{record_set_list=}")
+        record_with_min_usable_set = None
+        min_usable = None
+        for record in soon_to_expire_records:
+            record_set = self.get_set_for_record(record, record_set_list)
+            print(f"{record_set=}")
+            if min_usable is None or record_set.get("usable") < min_usable:
+                min_usable = record_set.get("usable")
+                record_with_min_usable_set = record
+
+        print(f"{record_with_min_usable_set=}")
+        if record_with_min_usable_set:
+            return record_with_min_usable_set.get("key")
+        return None
